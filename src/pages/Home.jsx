681652dfb6b2e5e62899457a1144e1b91eca2769
evgenia-taipova <evgenia.taipova@yandex.ru>
2025-02-02 15:57:56 +0100

    slider done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import OkIcon from "../assets/okay.svg";
 import ImgAdv from "../assets/Home/Component-1/3.svg";
 import TelegramIcon from "../assets/links/telegram.svg";
 import WhatsappIcon from "../assets/links/whatsapp.svg";
-
+import BgImage from "../assets/pages-bg/home.png";
 import TopBg from "../components/TopBg";
 import { useState } from "react";
 import { PhoneInput } from "react-international-phone";
@@ -11,6 +11,24 @@ import "react-international-phone/style.css";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import ArrowIcon from "../assets/arrow.svg";
+import ArrowNextIcon from "../assets/arrow-next.svg";
+import ArrowPrevIcon from "../assets/arrow-prev.svg";
+
+import englishForIt from "../assets/pages-bg/english-for-it.png";
+import englishForBusiness from "../assets/pages-bg/english-for-business.png";
+
+const NextArrow = ({ onClick }) => (
+  <button className="custom-arrow next" onClick={onClick}>
+    <img src={ArrowNextIcon} alt="Next" />
+  </button>
+);
+
+const PrevArrow = ({ onClick }) => (
+  <button className="custom-arrow prev" onClick={onClick}>
+    <img src={ArrowPrevIcon} alt="Previous" />
+  </button>
+);
 
 export function Home() {
   const [phone, setPhone] = useState("");
@@ -18,36 +36,142 @@ export function Home() {
     {
       id: 1,
       title: "English for IT",
+      category: "specialized",
       description:
         "Перетворіть англійську на інструмент для досягнення кар’єрних цілей завдяки онлайн- курсу для IT-спеціалістів.",
+      image: englishForIt,
     },
     {
       id: 2,
       title: "English for Business",
+      category: "specialized",
       description:
         "Опануйте ділову англійську, щоб впевнено спілкуватися з партнерами, вести переговори та досягати нових вершин у бізнесі.",
+      image: englishForBusiness,
     },
     {
       id: 3,
-      title: "English for Business",
+      title: "English for NGOs",
+      category: "specialized",
       description:
-        "Опануйте ділову англійську, щоб впевнено спілкуватися з партнерами, вести переговори та досягати нових вершин у бізнесі.",
+        "Опануйте англійську для ефективної роботи у громадських організаціях, захисту прав людини та підтримки демократії.",
+      image: "/src/assets/pages-bg/english-for-ngos.png",
     },
     {
       id: 4,
-      title: "English for Business",
+      title: "English for Media Literacy",
+      category: "specialized",
       description:
-        "Опануйте ділову англійську, щоб впевнено спілкуватися з партнерами, вести переговори та досягати нових вершин у бізнесі.",
+        "Дізнайтеся, як англійська допомагає орієнтуватися у сучасному світі медіа, критично оцінювати інформацію та розпізнавати маніпуляції.",
+      image: "../assets/pages-bg/english-for-media.png",
+    },
+    {
+      id: 5,
+      title: "English for Law",
+      category: "specialized",
+      description:
+        "Оволодійте англійською для юридичної сфери завдяки курсу, що зосереджується на правовій лексиці та практичних навичках.",
+      image: "../assets/pages-bg/english-for-law.png",
+    },
+
+    {
+      id: 6,
+      title: "English Grammar A2",
+      category: "grammar",
+      description:
+        "Покращуйте граматику англійської мови на рівні A2 за допомогою курсу для закріплення основних правил і структури мови.",
+      image: "../assets/pages-bg/english-grammar-a2.png",
+    },
+
+    {
+      id: 7,
+      title: "English Grammar B1+",
+      category: "grammar",
+      description:
+        "Поглиблюйте знання граматики англійської мови на рівні B1+ і вдосконалюйте навички використання мови.",
+      image: "../assets/pages-bg/english-grammar-b1.png",
+    },
+    {
+      id: 8,
+      title: "English Speaking Club (A2 Level)",
+      category: "speaking",
+      description:
+        "Відбувається раз на тиждень! Розмовний клуб англійської мови - це ідеальне місце для тих, хто хоче покращити свою розмовну англійську, позбутися мовного бар'єру та навчитися вільно висловлювати свої думки в повсякденних і професійних ситуаціях.",
+      image: "../assets/pages-bg/english-speaking-a2.png",
+    },
+    {
+      id: 9,
+      title: "English Speaking Club (B1+ Level)",
+      category: "speaking",
+      description:
+        "Відбувається раз на тиждень! Розмовний клуб англійської мови - це ідеальне місце для тих, хто хоче покращити свою розмовну англійську, позбутися мовного бар'єру та навчитися вільно висловлювати свої думки в повсякденних і професійних ситуаціях.",
+      image: "../assets/pages-bg/english-speaking-b1.png",
+    },
+
+    {
+      id: 10,
+      title: "English Speaking Club (B1+ Level, Business English)",
+      category: "speaking",
+      description:
+        "Відбувається раз на тиждень! Розмовний клуб англійської мови - це ідеальне місце для тих, хто хоче покращити свою розмовну англійську, позбутися мовного бар'єру та навчитися вільно висловлювати свої думки в повсякденних і професійних ситуаціях.",
+      image: "../assets/pages-bg/english-speaking-business.png",
+    },
+    {
+      id: 11,
+      title: "IT Professionals Speaking Club",
+      category: "speaking",
+      description:
+        "Відбувається раз на тиждень! Розмовний клуб англійської мови - це ідеальне місце для тих, хто хоче покращити свою розмовну англійську, позбутися мовного бар'єру та навчитися вільно висловлювати свої думки в повсякденних і професійних ситуаціях.",
+      image: "../assets/pages-bg/english-speaking-it.png",
     },
   ];
-  const settings = {
-    dots: true,
-    infinite: true,
+  const settings = (slidesCount) => ({
+    className: "slider variable-width",
+    dots: slidesCount > 2,
+    infinite: slidesCount > 2,
     speed: 500,
-    slidesToShow: 2,
+    slidesToShow: Math.min(slidesCount, 2),
     slidesToScroll: 1,
-    arrows: true,
+    nextArrow: slidesCount > 2 ? <NextArrow /> : null,
+    prevArrow: slidesCount > 2 ? <PrevArrow /> : null,
+    customPaging: () => <div className="custom-dot" />,
+    dotsClass: "slick-dots custom-dots",
+  });
+
+  const backgroundImage = "src/assets/pages-bg/english-for-law.png";
+
+  const CourseSlider = ({ title, category }) => {
+    const filteredCourses = courses.filter(
+      (course) => course.category === category
+    );
+    return (
+      <div className="slider-container">
+        <h2>{title}</h2>
+        <Slider {...settings(filteredCourses.length)}>
+          {filteredCourses.map((course) => (
+            <div
+              key={course.id}
+              className="slide"
+    
+            >
+              <div
+                className="slide-content"
+                style={course.id === 7 ? { paddingRight: "60px" } : {}}
+              >
+                <span>{`0${course.id}`}</span>
+                <h3>{course.title}</h3>
+                <p>{course.description}</p>
+              </div>
+              <button>
+                <img src={ArrowIcon} alt="" />
+              </button>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    );
   };
+
   return (
     <>
       <section className="home-top">
@@ -60,21 +184,13 @@ export function Home() {
             і розпочинайте шлях до змін вже зараз.
           </p>
         </div>
-        <div className="slider-container">
-          <h2>Спеціалізована англійська</h2>
-          <Slider {...settings}>
-            {courses.map((course) => (
-              <div key={course.id} className="slide">
-                <div className="slide-content">
-                  <span>{`0${course.id}`}</span>
-                  <h3>{course.title}</h3>
-                  <p>{course.description}</p>
-                </div>
-                {/* <button><img src={} alt="" /></button> */}
-              </div>
-            ))}
-          </Slider>
-        </div>
+        <CourseSlider
+          title="Спеціалізована англійська"
+          category="specialized"
+        />
+
+        <CourseSlider title="Граматика англійської мови" category="grammar" />
+        <CourseSlider title="English Speaking Club" category="speaking" />
 
         <TopBg />
       </section>
